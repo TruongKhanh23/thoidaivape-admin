145b373a792795e75a15d6905fab81be9ef83768
Trương Khánh <truongn.khanh@dai-ichi-life.com.vn>
2024-11-19 10:21:32 +0700

    TDVA-007: Fix TypeError: Failed to resolve module specifier "firebase/firestore". Relative references must start with either "/", "./", or "../".

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,35 +1,26 @@
-import { fileURLToPath, URL } from 'node:url';
-
+// vite.config.mjs
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    optimizeDeps: {
-        noDiscovery: true
-    },
     plugins: [
         vue(),
         Components({
             resolvers: [PrimeVueResolver()]
         })
     ],
-    server: {
-        port: 5176 // Thay đổi thành cổng bạn muốn, ví dụ: 3001
+    optimizeDeps: {
+        include: ['firebase/app', 'firebase/firestore', 'firebase/auth']
     },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    build: {
-        rollupOptions: {
-            external: [
-                'firebase/firestore', // Đánh dấu module là external để tránh lỗi
-                'firebase/app'
-            ]
-        }
+    server: {
+        port: 5176
     }
 });
